4e8cc057110da9a70516463969485de03805adfd
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-17 09:53:14 -0400

    Adding getters & setters for deposit, withdraw, balance

--- src/com/example/ModifiedATMProgram/ModifiedATMProgram.java
+++ src/com/example/ModifiedATMProgram/ModifiedATMProgram.java
@@ -7,4 +7,28 @@ public class ModifiedATMProgram {
     private double withdrawAmount;
     private double balanceAmount;
 
+    public void setDepositAmount(double userDeposit) {
+        depositAmount = userDeposit;
+    }
+
+    public void setWithdrawAmount(double userWithdraw) {
+        withdrawAmount = userWithdraw;
+    }
+
+    public void setBalanceAmount(double currentBalance) {
+        balanceAmount = currentBalance;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
+
+    public double getBalanceAmount() {
+        return balanceAmount;
+    }
+
 }
