163053d9a1ef08a1fb825f6bfc7f7f45b59d52c9
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-17 09:48:30 -0400

    Adding private doubles for deposit, withdraw, balance

--- src/com/example/ModifiedATMProgram/ModifiedATMProgram.java
+++ src/com/example/ModifiedATMProgram/ModifiedATMProgram.java
@@ -2,4 +2,9 @@ package com.example.ModifiedATMProgram;
 import java.util.Scanner;
 
 public class ModifiedATMProgram {
+
+    private double depositAmount;
+    private double withdrawAmount;
+    private double balanceAmount;
+
 }
