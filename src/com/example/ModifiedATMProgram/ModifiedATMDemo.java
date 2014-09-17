dc6c92628b1213701829d90a84e3433d91a8e1f2
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-17 10:04:40 -0400

    Instantiating new object myATM from class ModifiedATMProgram

--- src/com/example/ModifiedATMProgram/ModifiedATMDemo.java
+++ src/com/example/ModifiedATMProgram/ModifiedATMDemo.java
@@ -5,6 +5,7 @@ public class ModifiedATMDemo {
     public static void main(String[] args) {
 
     Scanner user = new Scanner(System.in);
+    ModifiedATMProgram myATM = new ModifiedATMProgram();
 
 
     }
